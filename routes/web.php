8bf6e6760e2569e299e478afcdfe99d6ec81043d
Shreya Subedi <subedishraya100@gmail.com>
2025-05-07 11:13:09 +0545

    first commit

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('testing',function(){
+    return view('test');
+
+});
