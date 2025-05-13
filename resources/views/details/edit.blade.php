370bfd40c5f55049dc844e664a875756cab7ad2a
Shreya Subedi <subedishraya100@gmail.com>
2025-05-13 12:43:31 +0545

    pay commit

--- resources/views/details/edit.blade.php
+++ resources/views/details/edit.blade.php
@@ -12,39 +12,40 @@
     @include('navbar')
     <div class="ml-80">
         <h1>Edit Details</h1>
-        <form action="{{ url('/users/' . $user->id . '/details/update') }}" method="POST" class="flex flex-col">
+        <form action="" method="POST" class="flex flex-col">
             @csrf
             @method('PUT')
 
             <div id="itemsContainer">
                 @foreach ($details as $detail)
-                <div class="flex gap-4 mb-2 items-center">
-                    <input type="hidden" name="detail_id[]" value="{{ $detail->id }}">
-                    <input type="text" value="{{ $detail->item }}" name="item[]" placeholder="Item" class="border border-gray-900 w-64">
-                    <input type="number" value="{{ $detail->amount }}" name="amount[]" placeholder="Amount" class="border border-gray-900 w-64">
+                    <div class="flex gap-4 mb-2 items-center">
+                        <input type="hidden" name="detail_id[]" value="{{ $detail->id }}">
+                        <input type="text" value="{{ $detail->item }}" name="item[]" placeholder="Item"
+                            class="border border-gray-900 w-64">
+                        <input type="number" value="{{ $detail->amount }}" name="amount[]" placeholder="Amount"
+                            class="border border-gray-900 w-64">
 
-                    <!-- Separate form for Delete button -->
-                    <form action="{{ route('details.destroy', $detail->id) }}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" 
-                            onclick="return confirm('Are you sure you want to delete this item?')" 
+                        <!-- Separate form for Delete button -->
+                        <button type="submit" onclick="return setUrl('delete', {{ $detail->id }})"
                             class="bg-red-500 text-white text-sm px-3 py-1 rounded hover:bg-red-600">
                             Delete
                         </button>
-                    </form>
 
-                </div>
+                    </div>
                 @endforeach
             </div>
 
-            <button type="button" onclick="addMoreItems()" class="bg-gray-600 text-white text-sm px-3 py-1 rounded hover:bg-gray-800 w-max mb-4">
+            <button type="button" onclick="addMoreItems()"
+                class="bg-gray-600 text-white text-sm px-3 py-1 rounded hover:bg-gray-800 w-max mb-4">
                 Add More Items
             </button>
 
-            <button type="submit" class="bg-black text-white text-sm px-3 py-1 rounded hover:bg-gray-800 w-max">
+            <button type="submit" onclick="return setUrl('update')"
+                class="bg-black text-white text-sm px-3 py-1 rounded hover:bg-gray-800 w-max">
+                
                 Update
             </button>
+
         </form>
 
         <script>
@@ -61,6 +62,28 @@
                     </button>
                 `;
                 container.appendChild(newItem);
+            }
+
+            function setUrl(action, detailId = null) {
+                const form = document.querySelector('form');
+                const methodInput = form.querySelector('input[name="_method"]');
+                if (action === 'delete' && detailId) {
+                    if (confirm("Are you sure you want to delete this detail?")) {
+                        form.action = `/details/${detailId}`;
+                        if (methodInput) {
+                            methodInput.value = 'DELETE';
+                        } else {
+                            const newMethodInput = document.createElement('input');
+                            newMethodInput.type = 'hidden';
+                            newMethodInput.name = '_method';
+                            newMethodInput.value = 'DELETE';
+                            form.appendChild(newMethodInput);
+                        }
+                        form.submit();
+                    }
+                } else {
+                    form.action = `/users/{{ $user->id }}/details/update`;
+                }
             }
         </script>
 
